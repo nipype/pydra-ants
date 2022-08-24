3a391a7183996c774e6abe3f3afb5e0bb05c7fcb
mahdieh-dst <mahdieh.dashtbanimoghari@sydney.edu.au>
2022-08-24 22:32:17 +1000

    Modified Preprocess.py

--- pydra/tasks/ants/Preprocess.py
+++ pydra/tasks/ants/Preprocess.py
@@ -2,7 +2,8 @@ import attr
 import typing as ty
 from pathlib import Path
 from pydra import ShellCommandTask
-from pydra.engine.specs import ShellSpec, ShellOutSpec, File, Path, Directory, SpecInfo
+from pydra.engine import specs
+from pydra.engine.specs import ShellSpec, ShellOutSpec, File, Path, Directory, SpecInfo, MultiInputFile
 
 # antsRegistration
 """The user can specify any number of "stages" where a stage 
@@ -14,12 +15,12 @@ from pydra.engine.specs import ShellSpec, ShellOutSpec, File, Path, Directory, S
 
 input_fields = [
     (
-        "dimensionality",
+        "dimension",
         int,
         {
-            "argstr": "--dimensionality '{dimensionality}'",
-            "help_string": ("forces the image to be treated as a specified-dimensional image"),
-            "allowed_values": ("2","3","4"), #1?"num"?---[]?
+            "argstr": "--dimensionality '{dimension}'",
+            "help_string": ("Image dimension"),
+            "allowed_values": [2,3,4], #1?list?
             "mandatory": True,
         },
     ),
@@ -42,7 +43,7 @@ input_fields = [
     ),
     (
         "restoreStateAsATransform",
-        File, #4? 
+        File, 
         {
             "argstr": "--restore-state '{restoreStateAsATransform}'",
             "help_string": "Specify the initial state of the registration which get immediately used to directly initialize the registration process",
@@ -58,7 +59,7 @@ input_fields = [
                 "should be written to an hdf5 composite file. This is false by default" 
                 "so that only the transform for each stage is written to file."
             ),
-            "allowed_values": ("0","1"),#7?
+            "allowed_values": [0,1],#7?
         },
     ),
     (
@@ -127,21 +128,156 @@ input_fields = [
         },
     ),
     (
-        "initialFixedTransform", 
-        ty.List[str], #9 file or list???? initialTransform,[initialTransform,<useInverse>],[fixedImage,movingImage,initializationFeature] 
+        "initial_fixed_transform", 
+        MultiInputFile, #9 ty.list[str]???? initialTransform,[initialTransform,<useInverse>],[fixedImage,movingImage,initializationFeature] 
         {
-            "argstr": "--initial-fixed-transform '{initialFixedTransform}'", 
+            "argstr": "--initial-fixed-transform '{initial_fixed_transform}'", 
             "help_string": (
-                "Specify the initial fixed transform(s) which get immediately incorporated into" 
-                "the composite transform. The order of the transforms is stack-esque in that the"
-                "last transform specified on the command line is the first to be applied."
+                "Specify the initial fixed transform(s) that should be applied before the registration begins."
+                "Note that, when a list is given, the transformations are applied in reverse order."
                 "the user can also perform an initial translation alignment by specifying the fixed and moving images"
                 "and selecting an initialization feature. These features include using the geometric center of the" 
                 "images (=0), the image intensities (=1), or the origin of the images (=2). "
             ),  
-            "allowed_values": ([],[])
+            "xor": [" "],#####
         },
     ),
+    (
+        "initial_moving_transform", 
+        MultiInputFile, #initialTransform,[initialTransform,<useInverse>],[fixedImage,movingImage,initializationFeature] 
+        {
+            "argstr": "--initial-moving-transform '{initial_moving_transform}'", 
+            "help_string": (
+                "Specify the initial moving transform(s) that should be applied before the registration begins."
+                "Note that, when a list is given, the transformations are applied in reverse order."
+                "the user can also perform an initial translation alignment by specifying the fixed and moving images"
+                "and selecting an initialization feature. These features include using the geometric center of the" 
+                "images (=0), the image intensities (=1), or the origin of the images (=2). "
+            ),  
+            "xor": [" "],#####
+        },
+    ),
+    (
+        "metric", #???C[fixedImage,movingImage,metricWeight,radius,...]  how to show this?
+        list,
+        {
+            "argstr": "{metric}",
+            "help_string": "the metric(s) to use for each stage. Note that multiple metrics per stage are not supported in ANTS 1.9.1 and earlier.",
+            "mandatory": True,
+            "allowed_values": ["CC","MI","Mattes","MeanSquares","Demons","GC"],
+        },
+    ),
+    (
+        "metric_weight",
+        list,
+        {
+            "help_string": "the metric weight(s) for each stage. The weights must sum to 1 per stage.",
+            "mandatory": True,
+            "requires": ["metric"],
+        },
+    ),
+    (
+        "radius_or_number_of_bins",
+        int, #?
+        {
+            "help_string": "the number of bins in each stage for the MI and Mattes metrics, the radius for other metrics",
+            "requires": ["metric"], #?
+        },
+    ),
+    (
+        "sampling_strategy", #<samplingStrategy={None,Regular,Random}>
+        list, #?
+        {
+            "help_string": "the metric sampling strategy (strategies) defined by a sampling percentage for each stage."
+            "The sampling strategy defaults to 'None' (aka a dense sampling of one sample per voxel), otherwise it defines a point set over" 
+            "which to optimize the metric. The point set can be on a regular lattice or a" 
+            "random lattice of points slightly perturbed to minimize aliasing artifacts.",
+            "requires": ["metric"],
+        },
+    ),
+    (
+        "sampling_percentage", #<samplingPercentage=[0,1]>
+        list, #? 
+        {
+            "help_string": "the metric sampling percentage(s) to use for each stage, it defines the fraction of points to select from the domain.",
+            "requires": ["metric"],
+        },
+    ),
+    (
+        "use_gradient_filter",
+        ty.Any,#?
+        False,#?default
+    {
+        "help_string": "specifies whether a smoothingfilter is applied when estimating the metric gradient.",
+    },
+    ),
+    (
+        "transforms",
+        list,
+        {"argstr": "{transforms}", "mandatory": True, "help_string": "transform type",},
+    ),
+    ("transform_parameters", list, {"help_string": "parameters are transform-specific and can be determined from the usage", "requires": ["transforms"],}),#??
+    ("number_of_iterations", list, {"help_string": ""}),#?list
+    (
+        "convergence",
+        list,#?
+        {"argstr": "--convergence '{convergence}'","help_string": "", "requires": ["number_of_iterations"]},
+    ),
+    (
+        "convergence_threshold",
+        list,
+        [1e-06],
+        {"help_string": "", "requires": ["number_of_iterations"]},
+    ),
+    (
+        "convergence_window_size",
+        list,
+        [10],
+        {"help_string": "", "requires": ["convergence_threshold"]},
+    ),
+    ("smoothing_sigmas", list, {"argstr":"--smoothing-sigmas '{smoothing_sigmas}'", "help_string": "", "mandatory": True}),
+    (
+        "sigma_units",
+        list,
+        {"help_string": "units for smoothing sigmas", "requires": ["smoothing_sigmas"]},
+    )
+    ("shrink_factors", list, {"argstr":"--shrink-factors '{shrink_factors}'","help_string": "", "mandatory": True}),
+    (
+        "use_histogram_matching",
+        ty.Any,
+        True,
+        {
+            "argstr":"--use-histogram-matching '{use_histogram_matching}'",
+            "help_string": "Histogram match the images before registration.",
+        },
+    ),
+    (
+        "winsorize_image_intensities",
+        ty.Any,
+        1.0,
+        {
+            "argstr":"--winsorize-image-intensities '{winsorize_image_intensities}'",
+            "help_string": "The Upper quantile to clip image ranges",
+        },
+    ),
+    (
+        float,
+        bool,
+        {
+            "help_string": "Use float instead of double for computations.",
+            "argstr": "--float {float}",
+        },
+    ),
+    (
+        "random_seed",
+        int,
+        {
+            "help_string": "Fixed seed for random number generation",
+            "argstr": "--random-seed {random_seed}",
+        },
+    ),
+    ("verbose", bool, False, {"argstr": "--verbose '{verbose}'", "help_string": "Verbose output",}),
+
 
 
 
