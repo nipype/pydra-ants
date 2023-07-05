ab92c64bf3f4b54cb9d2383069f28d23aa1a222b
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2023-07-05 10:47:59 +0200

    REF: Normalize ApplyTransforms with ANTsPy
    - input_image -> moving_image, reference_image -> fixed_image
    - image_type -> use and document the integer codes
    - float and verbose flags require a value of 0 or 1 for False and True

--- src/pydra/tasks/ants/apply_transforms.py
+++ src/pydra/tasks/ants/apply_transforms.py
@@ -5,32 +5,30 @@ ApplyTransforms
 Examples
 --------
 
->>> task = ApplyTransforms(input_image="moving.nii", reference_image="fixed.nii")
+>>> task = ApplyTransforms(moving_image="moving.nii", fixed_image="fixed.nii")
 >>> task.cmdline    # doctest: +ELLIPSIS
-'antsApplyTransforms --input-image-type 0 --input moving.nii --reference-image fixed.nii \
---output .../moving_warped.nii --interpolation Linear'
+'antsApplyTransforms -e scalar -i moving.nii -r fixed.nii -o .../moving_warped.nii -n Linear ...'
 
 >>> task = ApplyTransforms(
-...     input_image="moving.nii",
-...     reference_image="fixed.nii",
+...     moving_image="moving.nii",
+...     fixed_image="fixed.nii",
 ...     interpolation_method="BSpline",
 ...     transform_files=["affine.mat"],
 ... )
->>> task.cmdline
-'antsApplyTransforms ... --interpolation BSpline[3] --transform affine.mat'
+>>> task.cmdline    # doctest: +ELLIPSIS
+'antsApplyTransforms ... -n BSpline[3] -t affine.mat ...'
 
 >>> task = ApplyTransforms(
-...     input_image="moving.nii",
-...     reference_image="fixed.nii",
+...     moving_image="moving.nii",
+...     fixed_image="fixed.nii",
 ...     interpolation_method="Gaussian",
 ...     sigma=4.0,
 ...     alpha=1.0,
 ...     transform_files=["affine.mat", "warp_field.nii.gz"],
 ...     which_to_invert=[True, False],
 ... )
->>> task.cmdline
-'antsApplyTransforms ... --interpolation Gaussian[4.0, 1.0] --transform [affine.mat, 1] \
---transform [warp_field.nii.gz, 0]'
+>>> task.cmdline    # doctest: +ELLIPSIS
+'antsApplyTransforms ... -n Gaussian[4.0, 1.0] -t [affine.mat, 1] -t [warp_field.nii.gz, 0] ...'
 """
 
 __all__ = ["ApplyTransforms"]
@@ -51,7 +49,7 @@ class ApplyTransforms(ShellCommandTask):
         dimensionality: int = field(
             metadata={
                 "help_string": "force image dimensionality (2, 3 or 4)",
-                "argstr": "--image-dimensionality",
+                "argstr": "-d",
                 "allowed_values": {2, 3, 4},
             }
         )
@@ -60,43 +58,27 @@ class ApplyTransforms(ShellCommandTask):
             default="scalar",
             metadata={
                 "help_string": (
-                    "specify the image type (scalar, vector, tensor, time-series, multichannel, five-dimensional)"
+                    "specify the image type (0: scalar, 1: vector, 2: tensor, 3: time-series, 4: multichannel,"
+                    " 5: five-dimensional)"
                 ),
-                "allowed_values": {"scalar", "vector", "tensor", "time-series", "multichannel", "five-dimensional"},
-                "formatter": lambda image_type: (
-                    "--input-image-type {}".format(
-                        {
-                            "scalar": 0,
-                            "vector": 1,
-                            "tensor": 2,
-                            "time-series": 3,
-                            "multichannel": 4,
-                            "five-dimensional": 5,
-                        }.get(image_type)
-                    )
-                ),
+                "argstr": "-e",
+                "allowed_values": {0, 1, 2, 3, 4, 5},
             },
         )
 
-        input_image: PathLike = field(metadata={"help_string": "input image", "mandatory": True, "argstr": "--input"})
+        moving_image: PathLike = field(metadata={"help_string": "moving image", "mandatory": True, "argstr": "-i"})
 
-        reference_image: PathLike = field(
-            metadata={"help_string": "reference image", "mandatory": True, "argstr": "--reference-image"}
-        )
+        fixed_image: PathLike = field(metadata={"help_string": "fixed image", "mandatory": True, "argstr": "-r"})
 
         output_image: str = field(
-            metadata={
-                "help_string": "output image",
-                "argstr": "--output",
-                "output_file_template": "{input_image}_warped",
-            }
+            metadata={"help_string": "output image", "argstr": "-o", "output_file_template": "{moving_image}_warped"}
         )
 
         _interpolation = field(
             metadata={
                 "help_string": "interpolation",
                 "formatter": lambda interpolation_method, sigma, alpha, spline_order: (
-                    f"--interpolation {interpolation_method}"
+                    f"-n {interpolation_method}"
                     + (
                         f"[{spline_order}]"
                         if interpolation_method == "BSpline"
@@ -135,7 +117,7 @@ class ApplyTransforms(ShellCommandTask):
         output_datatype: str = field(
             metadata={
                 "help_string": "force output image datatype",
-                "argstr": "--output-data-type",
+                "argstr": "-u",
                 "allowed_values": {"char", "uchar", "short", "int", "float", "double", "default"},
             }
         )
@@ -146,9 +128,9 @@ class ApplyTransforms(ShellCommandTask):
                 "formatter": lambda transform_files, which_to_invert: (
                     ""
                     if not transform_files
-                    else " ".join(f"--transform {f}" for f in transform_files)
+                    else " ".join(f"-t {f}" for f in transform_files)
                     if not which_to_invert
-                    else " ".join(f"--transform [{f}, {int(i)}]" for f, i in zip(transform_files, which_to_invert))
+                    else " ".join(f"-t [{f}, {int(i)}]" for f, i in zip(transform_files, which_to_invert))
                 ),
             }
         )
@@ -157,15 +139,23 @@ class ApplyTransforms(ShellCommandTask):
 
         which_to_invert: Sequence[bool] = field(metadata={"help_string": "specify which transforms to invert"})
 
-        default_value: float = field(
-            metadata={"help_string": "default voxel value for input image", "argstr": "--default-value"}
-        )
+        default_value: float = field(metadata={"help_string": "default voxel value", "argstr": "-f"})
 
         use_float_precision: bool = field(
-            metadata={"help_string": "use float precision instead of double", "argstr": "--float"}
+            default=False,
+            metadata={
+                "help_string": "use float precision instead of double",
+                "formatter": lambda use_float_precision: f"--float {int(use_float_precision)}",
+            },
         )
 
-        verbose: bool = field(metadata={"help_string": "enable verbose output", "argstr": "--verbose"})
+        verbose: bool = field(
+            default=False,
+            metadata={
+                "help_string": "enable verbose output",
+                "formatter": lambda verbose: f"--verbose {int(verbose)}",
+            },
+        )
 
     input_spec = SpecInfo(name="Input", bases=(InputSpec,))
 
