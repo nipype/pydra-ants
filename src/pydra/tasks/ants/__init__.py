edbc9723254a66e8b6ae8d957609604051c20222
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2023-07-12 19:18:01 +0200

    NEW: Add task for CreateJacobianDeterminantImage

--- src/pydra/tasks/ants/__init__.py
+++ src/pydra/tasks/ants/__init__.py
@@ -7,4 +7,5 @@ ANTs
 
 from .apply_transforms import ApplyTransforms
 from .bias_correction import N4BiasFieldCorrection
+from .create_jacobian_determinant_image import CreateJacobianDeterminantImage
 from .registration import RegistrationSyNQuick
