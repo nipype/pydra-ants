d95abe3e77432ebce0a8067e895ac04826fef952
Ghislain Vaillant <ghisvail@users.noreply.github.com>
2023-07-23 16:26:12 -0400

    NEW: Add task for antsRegistration (#2)
    * NEW: Add task for antsRegistration
    
    * MNT: Run lint fix
    
    * ENH: Simplify output parameters
    
    * ENH: Improve defaults of registration task
    
    * ENH: Fix docstring for rigid stage flag
    
    * REF: Better affine metric
    
    * REF: Better rigid metric
    
    * ENH: Drop dimensionality, handled automatically.
    
    * ENH: Add fixed and moving masks
    
    * ENH: Expose affine transform, forward and inverse warp fields.
    
    * NEW: Add registration_syn_quick wrapper
    
    * ENH: Add initial stage to Registration
    
    * NEW: Add registration_syn wrapper
    
    - Add `quick` parameter to `registration_syn`
    - Turn `registration_syn_quick` into a partial application
    
    * ENH: Default is_large_image to True
    
    Based on the code from antsRegistrationSyn, an image is considered "large" if
    any of its dimensions is larger than 256. This is very likely to happen.
    
    * ENH: Add initial transforms for fixed and moving images
    
    * DOC: Add docstring to registration_syn
    
    * ENH: Add GaussianDisplacementField to SyN transforms
    
    Follows what ANTsPy is doing in its registration wrapper.
    
    * ENH: More output parameters for registration_syn
    
    * FIX: Order of parameters in output formatter
    
    * ENH: Use output_prefix as in ANTs wrapper scripts
    
    * DOC: Add Returns section in docstring
    
    * FIX: Test case with default outputs
    
    * ENH: Simplify spline distance parameters
    
    - Use only one spline distance parameter for the flow field
    - Set the spline distance for the total field to 0
    
    * ENH: use_reproducible_mode -> reproducible
    
    * FIX: Change type of output fields: str -> File
    
    * ENH: Switch default of initialize_transforms_per_stage to False
    
    * FIX: Missing dimensionality parameter and formatting fixes
    
    * FIX: doctests
    
    * ENH: Drop collapse_output_transform
    
    Makes no sense to override it with the current design
    
    * ENH: Drop write_composite_transform
    
    Not needed for the current implementation.
    
    * DOC: Add example for default call to registration_syn
    
    * ENH: Switch is_large_image default to False
    
    * ENH: Move some fields around
    
    * ENH: Winsorize image intensities in registration_syn
    
    * ENH: Add parameter to save transforms to MINC format
    
    * ENH: Turn precision switch to boolean
    
    * ENH: Support MINC format in registration_syn
    
    * FIX: affine_transform -> transform_file, already used.
    
    * ENH: interpolation -> interpolator
    
    * ENH: Use warped_image and inverse_warped_image as output names
    
    * FIX: formatter of interpolation parameter
    
    * ENH: transform -> transform_type where applicable
    
    * ENH: Improve affine_transform output
    
    * ENH: More doctest for quick, large and reproducible modes
    
    * ENH: is_large_image -> large
    
    * MNT: lint fix

--- src/pydra/tasks/ants/__init__.py
+++ src/pydra/tasks/ants/__init__.py
@@ -8,4 +8,4 @@ ANTs
 from .apply_transforms import ApplyTransforms
 from .bias_correction import N4BiasFieldCorrection
 from .create_jacobian_determinant_image import CreateJacobianDeterminantImage
-from .registration import RegistrationSyNQuick
+from .registration import Registration, registration_syn, registration_syn_quick
