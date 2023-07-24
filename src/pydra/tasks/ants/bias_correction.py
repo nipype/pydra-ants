23f8da0235fd8566e919627ca100003363516b64
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2023-07-24 16:06:59 -0400

    ENH: Use short argument formatting for N4BFC

--- src/pydra/tasks/ants/bias_correction.py
+++ src/pydra/tasks/ants/bias_correction.py
@@ -7,9 +7,8 @@ Examples
 
 >>> task = N4BiasFieldCorrection(input_image="input.nii")
 >>> task.cmdline    # doctest: +ELLIPSIS
-'N4BiasFieldCorrection --input-image input.nii --rescale-intensities 1 --shrink-factor 4 \
---bspline-fitting [200, 3] --convergence [50x50x50x50, 0.0] --histogram-sharpening [0.15, 0.01, 200] \
---output .../input_corrected.nii'
+'N4BiasFieldCorrection -i input.nii -r 1 -s 4 -b [200,3] -c [50x50x50x50,0.0] -t [0.15,0.01,200] \
+-o .../input_corrected.nii'
 """
 
 __all__ = ["N4BiasFieldCorrection"]
@@ -28,35 +27,29 @@ class N4BiasFieldCorrection(ShellCommandTask):
     @define(kw_only=True)
     class InputSpec(ShellSpec):
         dimensionality: int = field(
-            metadata={
-                "help_string": "force image dimensionality (2, 3 or 4)",
-                "argstr": "--image-dimensionality",
-                "allowed_values": {2, 3, 4},
-            }
+            metadata={"help_string": "image dimensionality", "argstr": "-d", "allowed_values": {2, 3, 4}}
         )
 
-        input_image: PathLike = field(
-            metadata={"help_string": "input image", "mandatory": True, "argstr": "--input-image"}
-        )
+        input_image: PathLike = field(metadata={"help_string": "input image", "mandatory": True, "argstr": "-i"})
 
-        mask_image: PathLike = field(metadata={"help_string": "mask image", "argstr": "--mask-image"})
+        mask_image: PathLike = field(metadata={"help_string": "mask image", "argstr": "-x"})
 
         rescale_intensities: bool = field(
             default=True,
             metadata={
                 "help_string": "rescale intensities",
-                "formatter": lambda rescale_intensities: f"--rescale-intensities {int(rescale_intensities)}",
+                "formatter": lambda rescale_intensities: f"-r {rescale_intensities:d}",
             },
         )
 
-        weight_image: PathLike = field(metadata={"help_string": "weight image", "argstr": "--weight-image"})
+        weight_image: PathLike = field(metadata={"help_string": "weight image", "argstr": "-w"})
 
-        shrink_factor: int = field(default=4, metadata={"help_string": "shrink factor", "argstr": "--shrink-factor"})
+        shrink_factor: int = field(default=4, metadata={"help_string": "shrink factor", "argstr": "-s"})
 
-        bspline_fitting_parameters: str = field(
+        bspline_fitting_: str = field(
             metadata={
-                "help_string": "b-spline fitting parameters",
-                "argstr": "--bspline-fitting [{spline_distance}, {spline_order}]",
+                "help_string": "b-spline fitting parameter",
+                "argstr": "-b [{spline_distance},{spline_order}]",
                 "readonly": True,
             }
         )
@@ -65,12 +58,12 @@ class N4BiasFieldCorrection(ShellCommandTask):
 
         spline_order: int = field(default=3, metadata={"help_string": "spline order"})
 
-        convergence_parameters: str = field(
+        convergence_: str = field(
             metadata={
                 "help_string": "convergence parameters",
                 "readonly": True,
-                "formatter": lambda num_iterations, convergence_threshold: (
-                    "--convergence [{}, {}]".format("x".join(str(i) for i in num_iterations), convergence_threshold)
+                "formatter": lambda num_iterations, threshold: (
+                    "-c [{},{}]".format("x".join(str(i) for i in num_iterations), threshold)
                 ),
             }
         )
@@ -79,12 +72,12 @@ class N4BiasFieldCorrection(ShellCommandTask):
             default=(50, 50, 50, 50), metadata={"help_string": "number of iterations"}
         )
 
-        convergence_threshold: float = field(default=0.0, metadata={"help_string": "convergence threshold"})
+        threshold: float = field(default=0.0, metadata={"help_string": "convergence threshold"})
 
-        histogram_sharpening_parameters: str = field(
+        histogram_sharpening_: str = field(
             metadata={
-                "help_string": "histogram sharpening parameters",
-                "argstr": "--histogram-sharpening [{bias_field_fwhm}, {wiener_filter_noise}, {num_histogram_bins}]",
+                "help_string": "histogram sharpening parameter",
+                "argstr": "-t [{bias_field_fwhm},{wiener_filter_noise},{num_histogram_bins}]",
                 "readonly": True,
             }
         )
@@ -95,12 +88,12 @@ class N4BiasFieldCorrection(ShellCommandTask):
 
         num_histogram_bins: int = field(default=200, metadata={"help_string": "number of histogram bins"})
 
-        output_parameters = field(
+        output_ = field(
             metadata={
                 "help_string": "output parameters",
                 "readonly": True,
                 "formatter": lambda output_image, save_bias_field, output_bias_field: (
-                    f"--output [{output_image}, {output_bias_field}]" if save_bias_field else f"--output {output_image}"
+                    f"-o [{output_image},{output_bias_field}]" if save_bias_field else f"-o {output_image}"
                 ),
             }
         )
@@ -109,14 +102,10 @@ class N4BiasFieldCorrection(ShellCommandTask):
             metadata={"help_string": "output image", "output_file_template": "{input_image}_corrected"}
         )
 
-        save_bias_field: bool = field(metadata={"help_string": "save bias field"})
+        save_bias_field: bool = field(default=False, metadata={"help_string": "save bias field"})
 
         output_bias_field: str = field(
-            metadata={
-                "help_string": "output bias field",
-                "output_file_template": "{input_image}_biasfield",
-                "requires": {"save_bias_field"},
-            }
+            metadata={"help_string": "output bias field", "output_file_template": "{input_image}_biasfield"}
         )
 
     input_spec = SpecInfo(name="Input", bases=(InputSpec,))
