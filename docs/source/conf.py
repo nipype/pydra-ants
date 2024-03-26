0e553b55a3f6b27a18ca5dad69a3ebe08f92dc67
Tom Close <tom.g.close@gmail.com>
2024-03-26 14:39:21 +1100

    updated docs with aramis-style

---
+++ docs/source/conf.py
@@ -0,0 +1,26 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+from pydra.tasks.freesurfer import __version__  # noqa
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "pydra-ants"
+author = "Pydra Developers"
+copyright = f"2022-2023, {author}"
+release = __version__
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.autosummary", "sphinx.ext.napoleon"]
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "pydata_sphinx_theme"
---
+++ docs/source/index.rst
@@ -0,0 +1,24 @@
+About pydra-ants
+================
+
+`Pydra`_ is a dataflow engine which provides a set of lightweight abstractions for DAG construction, manipulation, and distributed execution.
+
+`ANTs`_ is an open source package for the analysis and visualization of structural, functional, and diffusion neuroimaging data from cross-sectional and longitudinal studies.
+
+pydra-ants provides Pydra tasks for running and composing FreeSurfer commands.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   api
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+.. _Pydra: https://nipype.github.io/pydra/
+.. _ANTs: https://github.com/ANTsX/ANTs
